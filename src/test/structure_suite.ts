96c87d1d9a4630461a189e482d82248f73b257da
VasiliyLysokobylko <vasily@onpositive.com>
2016-11-01 17:09:50 +0600

    Outline tests set

--- src/test/structure_suite.ts
+++ src/test/structure_suite.ts
@@ -12,6 +12,156 @@ describe('',function(){
         this.timeout(15000);
         testSuiteUtil.testOutline("structure/test001/api.raml");
     });
+
+    it("test002/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test002/api.raml");
+    });
+
+    it("test003/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test003/api.raml");
+    });
+
+    it("test004/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test004/api.raml");
+    });
+
+    it("test005/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test005/api.raml");
+    });
+
+    it("test006/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test006/api.raml");
+    });
+
+    it("test007/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test007/api.raml");
+    });
+
+    it("test008/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test008/api.raml");
+    });
+
+    it("test009/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test009/api.raml");
+    });
+
+    it("test010/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test010/api.raml");
+    });
+
+    it("test011/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test011/api.raml");
+    });
+
+    it("test012/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test012/api.raml");
+    });
+
+    it("test013/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test013/api.raml");
+    });
+
+    it("test014/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test014/api.raml");
+    });
+
+    it("test015/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test015/api.raml");
+    });
+
+    it("test016/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test016/api.raml");
+    });
+
+    it("test017/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test017/api.raml");
+    });
+
+    it("test018/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test018/api.raml");
+    });
+
+    it("test019/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test019/api.raml");
+    });
+
+    it("test020/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test020/api.raml");
+    });
+
+    it("test021/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test021/api.raml");
+    });
+
+    it("test022/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test022/api.raml");
+    });
+
+    it("test023/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test023/api.raml");
+    });
+
+    it("test024/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test024/api.raml");
+    });
+
+    it("test025/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test025/api.raml");
+    });
+
+    it("test026/overlay.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test026/overlay.raml", null, "structure/test026/api-outline.json");
+    });
+
+    it("test027/extension.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test027/extension.raml", null, "structure/test027/api-outline.json");
+    });
+
+    it("test028/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test028/api.raml");
+    });
+
+    it("test029/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test029/api.raml");
+    });
+
+    it("test030/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test030/api.raml");
+    });
+
+    it("test031/api.raml", function () {
+        this.timeout(15000);
+        testSuiteUtil.testOutline("structure/test031/api.raml");
+    });
     
 });
 
